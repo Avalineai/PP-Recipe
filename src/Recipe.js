bf84195f80ab6fe7300209aaf1d3eb28eb63adac
Calypso Blue <46878997+Avalineai@users.noreply.github.com>
2019-06-16 21:10:46 -0700

    added files

---
+++ src/Recipe.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import style from './recipe.module.css';
+
+const Recipe = ({title, calories, image, ingredients, instructions}) => {
+    return(
+        <div className={style.recipe}>
+            <h1>{title}</h1>
+            <ul>
+                {ingredients.map(ingredient =>(
+                    <li>
+                        {ingredient.text}
+                    </li>
+                ))}
+            </ul>
+            <p>Calories: {calories}</p>
+            <a href={instructions}>Instructions</a>
+            <img className={style.image} src={image} alt=""/>
+        </div>
+    );
+}
+
+export default Recipe;
